1383d32c26fff75a7f073c7a6b25cbe7634540a1
SmoQ <smoku250@gmail.com>
2023-07-18 07:51:16 +0200

    created the ocr git

---
+++ OCR.py
@@ -0,0 +1,16 @@
+import pytesseract
+from PIL import Image
+from fpdf import FPDF
+
+img_file = "new_file_to_ocr.png"
+img_file2 = 'File_to_ocr.png'
+
+img = Image.open(img_file2)
+
+ocr_result = pytesseract.image_to_pdf_or_hocr(img_file2, extension='pdf')
+
+
+if __name__ == '__main__':
+    print(ocr_result)
+    with open('ocr_topdf.pdf', 'wb') as file:
+        file.write(ocr_result)
